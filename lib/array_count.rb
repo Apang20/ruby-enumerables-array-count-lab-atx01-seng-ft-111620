048e51a28fb3614fbe5c58159a404eb751e40ae8
convivial-parser-7775 <avelonpang@gmail.com>
2020-10-06 23:59:45 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,15 +4,12 @@ def count_strings(array)
 #array.count do |element|
 #element.class == string 
 
+
   
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 
-#array.count do|element|
-#element == ""
-#total += 1 
-#end
-#total
+
 end 
